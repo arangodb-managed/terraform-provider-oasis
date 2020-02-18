fb0943234a0c848dbf2d9128ac124c1c9f7099bc
Gergely Brautigam <gergely@arangodb.com>
2020-02-18 09:47:15 +0100

    Added all the time variants

--- pkg/resource_backup_policy_test.go
+++ pkg/resource_backup_policy_test.go
@@ -19,28 +19,37 @@ import (
 	backup "github.com/arangodb-managed/apis/backup/v1"
 )
 
-func TestFlattenBackupPolicyWithHourlySchedule(t *testing.T) {
+func TestFlattenBackupPolicy(t *testing.T) {
+
 	policy := &backup.BackupPolicy{
-		Name:         "test-policy",
-		Description:  "test-description",
-		DeploymentId: "123456",
-		IsPaused:     true,
-		Schedule: &backup.BackupPolicy_Schedule{
-			ScheduleType: "hourly",
-			HourlySchedule: &backup.BackupPolicy_HourlySchedule{
-				ScheduleEveryIntervalHours: 10,
-			},
-		},
+		Name:              "test-policy",
+		Description:       "test-description",
+		DeploymentId:      "123456",
+		IsPaused:          true,
 		Upload:            true,
 		RetentionPeriod:   types.DurationProto(200 * 24 * time.Hour),
 		EmailNotification: "None",
 	}
+
 	expected := map[string]interface{}{
-		backupPolicyNameFieldName:         "test-policy",
-		backupPolicyDescriptionFieldName:  "test-description",
-		backupPolicyDeploymentIDFieldName: "123456",
-		backupPolicyIsPausedFieldName:     true,
-		backupPolicyScheduleFieldName: []interface{}{
+		backupPolicyNameFieldName:              "test-policy",
+		backupPolicyDescriptionFieldName:       "test-description",
+		backupPolicyDeploymentIDFieldName:      "123456",
+		backupPolicyIsPausedFieldName:          true,
+		backupPolicyUploadFieldName:            true,
+		backupPolicyRetentionPeriodFieldName:   200,
+		backupPolictEmailNotificationFeidlName: "None",
+	}
+
+	t.Run("with hourly schedule", func(tt *testing.T) {
+		schedule := &backup.BackupPolicy_Schedule{
+			ScheduleType: "hourly",
+			HourlySchedule: &backup.BackupPolicy_HourlySchedule{
+				ScheduleEveryIntervalHours: 10,
+			},
+		}
+		policy.Schedule = schedule
+		expectedSchedule := []interface{}{
 			map[string]interface{}{
 				backupPolicyScheduleTypeFieldName: "hourly",
 				backupPolicyScheudleHourlyScheduleFieldName: []interface{}{
@@ -49,11 +58,89 @@ func TestFlattenBackupPolicyWithHourlySchedule(t *testing.T) {
 					},
 				},
 			},
-		},
-		backupPolicyUploadFieldName:            true,
-		backupPolicyRetentionPeriodFieldName:   200,
-		backupPolictEmailNotificationFeidlName: "None",
-	}
-	flattened := flattenBackupPolicyResource(policy)
-	assert.Equal(t, expected, flattened)
+		}
+		expected[backupPolicyScheduleFieldName] = expectedSchedule
+		flattened := flattenBackupPolicyResource(policy)
+		assert.Equal(t, expected, flattened)
+	})
+	t.Run("with daily schedule", func(tt *testing.T) {
+		schedule := &backup.BackupPolicy_Schedule{
+			ScheduleType: "daily",
+			DailySchedule: &backup.BackupPolicy_DailySchedule{
+				Monday:    true,
+				Tuesday:   false,
+				Wednesday: false,
+				Thursday:  true,
+				Friday:    false,
+				Saturday:  false,
+				Sunday:    false,
+				ScheduleAt: &backup.TimeOfDay{
+					Hours:    10,
+					Minutes:  10,
+					TimeZone: "UTC",
+				},
+			},
+		}
+		policy.Schedule = schedule
+		expectedSchedule := []interface{}{
+			map[string]interface{}{
+				backupPolicyScheduleTypeFieldName: "daily",
+				backupPolicyScheudleDailyScheduleFieldName: []interface{}{
+					map[string]interface{}{
+						backupPolicyScheudleDailyScheduleMondayFieldName:    true,
+						backupPolicyScheudleDailyScheduleTuesdayFieldName:   false,
+						backupPolicyScheudleDailyScheduleWednesdayFieldName: false,
+						backupPolicyScheudleDailyScheduleThursdayFieldName:  true,
+						backupPolicyScheudleDailyScheduleFridayFieldName:    false,
+						backupPolicyScheudleDailyScheduleSaturdayFieldName:  false,
+						backupPolicyScheudleDailyScheduleSundayFieldName:    false,
+						backupPolicyTimeOfDayScheduleAtFieldName: []interface{}{
+							map[string]interface{}{
+								backupPolicyTimeOfDayHoursFieldName:    10,
+								backupPolicyTimeOfDayMinutesFieldName:  10,
+								backupPolicyTimeOfDayTimeZoneFieldName: "UTC",
+							},
+						},
+					},
+				},
+			},
+		}
+		expected[backupPolicyScheduleFieldName] = expectedSchedule
+		flattened := flattenBackupPolicyResource(policy)
+		assert.Equal(t, expected, flattened)
+	})
+	t.Run("with monthly schedule", func(tt *testing.T) {
+		schedule := &backup.BackupPolicy_Schedule{
+			ScheduleType: "monthly",
+			MonthlySchedule: &backup.BackupPolicy_MonthlySchedule{
+				DayOfMonth: 30,
+				ScheduleAt: &backup.TimeOfDay{
+					Hours:    10,
+					Minutes:  10,
+					TimeZone: "UTC",
+				},
+			},
+		}
+		policy.Schedule = schedule
+		expectedSchedule := []interface{}{
+			map[string]interface{}{
+				backupPolicyScheduleTypeFieldName: "monthly",
+				backupPolicyScheudleMonthlyScheduleFieldName: []interface{}{
+					map[string]interface{}{
+						backupPolicyScheudleMonthlyScheduleDayOfMonthScheduleFieldName: 30,
+						backupPolicyTimeOfDayScheduleAtFieldName: []interface{}{
+							map[string]interface{}{
+								backupPolicyTimeOfDayHoursFieldName:    10,
+								backupPolicyTimeOfDayMinutesFieldName:  10,
+								backupPolicyTimeOfDayTimeZoneFieldName: "UTC",
+							},
+						},
+					},
+				},
+			},
+		}
+		expected[backupPolicyScheduleFieldName] = expectedSchedule
+		flattened := flattenBackupPolicyResource(policy)
+		assert.Equal(t, expected, flattened)
+	})
 }
