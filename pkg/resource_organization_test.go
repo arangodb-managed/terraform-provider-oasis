714d36ecc4730c7134fb185dd2a5a8f94ca93094
Adonis Murati <adonis.murati@arangodb.com>
2022-04-27 14:42:39 +0200

    OAS-4766 | add unit and acceptance tests to organization resource

--- pkg/resource_organization_test.go
+++ pkg/resource_organization_test.go
@@ -19,3 +19,89 @@
 //
 
 package pkg
+
+import (
+	"fmt"
+	rm "github.com/arangodb-managed/apis/resourcemanager/v1"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"regexp"
+	"testing"
+)
+
+// TestAccResourceOrganization verifies the Oasis Organization resource is created along with the specified properties
+func TestAccResourceOrganization(t *testing.T) {
+	if _, ok := os.LookupEnv("TF_ACC"); !ok {
+		t.Skip()
+	}
+	t.Parallel()
+
+	res := "terraform-organization-" + acctest.RandString(10)
+	name := "organization-" + acctest.RandString(10)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testProviderFactories,
+		CheckDestroy:      testAccCheckDestroyDeployment,
+		Steps: []resource.TestStep{
+			{
+				Config:      testOrganizationConfig(res, ""),
+				ExpectError: regexp.MustCompile("unable to find parse field name"),
+			},
+			{
+				Config: testOrganizationConfig(res, name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("oasis_organization."+res, organizationNameFieldName, name),
+					resource.TestCheckResourceAttr("oasis_organization."+res, organizationDescriptionFieldName, "A test Oasis organization within from Terraform Provider"),
+				),
+			},
+		},
+	})
+}
+
+// testOrganizationConfig contains the Terraform resource definitions for testing usage
+func testOrganizationConfig(res, name string) string {
+	return fmt.Sprintf(`resource "oasis_organization" "%s" {
+  name        = "%s"
+  description = "A test Oasis organization within from Terraform Provider"
+}
+`, res, name)
+}
+
+// TestFlattenOrganization tests the Oasis Organization flattening for Terraform schema compatibility.
+func TestFlattenOrganization(t *testing.T) {
+	organization := &rm.Organization{
+		Name:        "test-organization",
+		Description: "test-description",
+	}
+
+	expected := map[string]interface{}{
+		organizationNameFieldName:        "test-organization",
+		organizationDescriptionFieldName: "test-description",
+	}
+
+	flattened := flattenOrganizationResource(organization)
+	assert.Equal(t, expected, flattened)
+}
+
+// TestExpandOrganization tests the Oasis Backup expansion for Terraform schema compatibility.
+func TestExpandOrganization(t *testing.T) {
+	raw := map[string]interface{}{
+		organizationNameFieldName:        "test-organization",
+		organizationDescriptionFieldName: "test-description",
+	}
+	expected := &rm.Organization{
+		Name:        "test-organization",
+		Description: "test-description",
+	}
+
+	s := resourceDeployment().Schema
+	resourceData := schema.TestResourceDataRaw(t, s, raw)
+	expandedOrganization, err := expandOrganizationResource(resourceData)
+	assert.NoError(t, err)
+
+	assert.Equal(t, expected, expandedOrganization)
+}
