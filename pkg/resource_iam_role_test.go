edba14662ecd50764f39f155e8090e4e6a63c2a5
Adonis Murati <adonis.murati@arangodb.com>
2022-05-04 10:21:46 +0200

    OAS-4770 | reorder imports in tests

--- pkg/resource_iam_role_test.go
+++ pkg/resource_iam_role_test.go
@@ -22,18 +22,18 @@ package pkg
 
 import (
 	"fmt"
-	common "github.com/arangodb-managed/apis/common/v1"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/stretchr/testify/require"
 	"os"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
+	common "github.com/arangodb-managed/apis/common/v1"
 	iam "github.com/arangodb-managed/apis/iam/v1"
 )
 
