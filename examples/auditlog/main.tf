99ebca620bffa71bb939a1977991c2642ea43056
Adonis Murati <adonis.murati@arangodb.com>
2022-05-19 15:51:37 +0200

    OAS-4763 | update comment in examples tf file

--- examples/auditlog/main.tf
+++ examples/auditlog/main.tf
@@ -13,7 +13,7 @@ provider "oasis" {
   api_key_secret = "" // API Key Secret generated in Oasis platform
 }
 
-// Terraform created organization
+// Terraform created auditlog
 resource "oasis_auditlog" "oasis_test_auditlog" {
   name        = "Terraform Oasis AuditLog"
   description = "A test Oasis auditlog from Terraform Provider"
