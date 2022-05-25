9c462d63b2c37cc1e5551b947ec190edb78cb17b
Adonis Murati <adonis.murati@arangodb.com>
2022-05-25 11:17:57 +0200

    OAS-4846 | add descriptions for auditlog fields

--- pkg/resource_auditlog.go
+++ pkg/resource_auditlog.go
@@ -43,26 +43,31 @@ const (
 // resourceAuditLog defines an Oasis Audit Log resource.
 func resourceAuditLog() *schema.Resource {
 	return &schema.Resource{
+		Description:   "Oasis Audit Log Resource",
 		CreateContext: resourceAuditLogCreate,
 		ReadContext:   resourceAuditLogRead,
 		UpdateContext: resourceAuditLogUpdate,
 		DeleteContext: resourceAuditLogDelete,
 		Schema: map[string]*schema.Schema{
 			auditLogNameFieldName: {
-				Type:     schema.TypeString,
-				Required: true,
+				Type:        schema.TypeString,
+				Description: "Audit Log Resource Audit Log Name",
+				Required:    true,
 			},
 			auditLogDescriptionFieldName: {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:        schema.TypeString,
+				Description: "Audit Log Resource Audit Log Description",
+				Optional:    true,
 			},
 			auditLogOrganizationFieldName: {
-				Type:     schema.TypeString,
-				Optional: true,
+				Type:        schema.TypeString,
+				Description: "Audit Log Resource Audit Log Organization ID",
+				Optional:    true,
 			},
 			auditLogIsDefaultFieldName: {
-				Type:     schema.TypeBool,
-				Optional: true,
+				Type:        schema.TypeBool,
+				Description: "Audit Log Resource Audit Log Is Default condition",
+				Optional:    true,
 			},
 		},
 	}
