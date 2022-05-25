2e9923c9a3ee7cd08ad3c38d8d6063725616b550
Adonis Murati <adonis.murati@arangodb.com>
2022-05-25 17:41:59 +0200

    OAS-4846 | add project resource to docs

--- examples/organization/outputs.tf
+++
@@ -1,9 +0,0 @@
-// Load in an Oasis organization
-data "oasis_organization" "oasis_test_organization" {
-  id = oasis_organization.oasis_test_organization.id // provide your organization id here
-}
-
-// Output the data after it has been synced.
-output "datasets" {
-  value = data.oasis_organization.oasis_test_organization
-}
