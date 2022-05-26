f9c2cbf5483350587483d7f4e99769c49c2f9cab
Adonis Murati <adonis.murati@arangodb.com>
2022-05-26 11:44:27 +0200

    OAS-4846 | update terms and conditions data source tf file with provider config

--- examples/data-sources/oasis_terms_and_conditions/data-source.tf
+++ examples/data-sources/oasis_terms_and_conditions/data-source.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    oasis = {
+      source  = "arangodb.com/managed/oasis"
+      version = ">=1.5.1"
+    }
+  }
+}
+
+provider "oasis" {
+  api_key_id     = "" // API Key ID generated in Oasis platform
+  api_key_secret = "" // API Key Secret generated in Oasis platform
+}
+
 // Load in Oasis terms and conditions
 data "oasis_terms_and_conditions" "test_terms_and_conditions" {
 }
