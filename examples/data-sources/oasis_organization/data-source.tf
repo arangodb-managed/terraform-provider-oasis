65cb46b9476895c887e93139290a6dbc5675f726
Adonis Murati <adonis.murati@arangodb.com>
2022-05-26 11:27:48 +0200

    OAS-4846 | update organization data source with provider config

--- examples/data-sources/oasis_organization/data-source.tf
+++ examples/data-sources/oasis_organization/data-source.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    oasis = {
+      source  = "arangodb.com/managed/oasis"
+      version = ">=1.5.1"
+    }
+  }
+}
+
+provider "oasis" {
+  api_key_id     = "" // API Key ID generated in Oasis platform
+  api_key_secret = "" // API Key Secret generated in Oasis platform
+}
+
 // Load in an Oasis organization
 data "oasis_organization" "oasis_test_organization" {
   id = "" // Provide your Organization ID here
