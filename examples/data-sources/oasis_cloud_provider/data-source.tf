50f5c8a101c5fcb0b99755a028a45a84a6d336ac
Adonis Murati <adonis.murati@arangodb.com>
2022-05-26 11:05:25 +0200

    OAS-4846 | update docs for cloud provider data source

--- examples/data-sources/oasis_cloud_provider/data-source.tf
+++ examples/data-sources/oasis_cloud_provider/data-source.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    oasis = {
+      source  = "arangodb.com/managed/oasis"
+      version = ">=1.5.1"
+    }
+  }
+}
+
+provider "oasis" {
+  api_key_id     = "" // API Key ID generated in Oasis platform
+  api_key_secret = "" // API Key Secret generated in Oasis platform
+}
+
 // Load in Oasis Cloud Providers
 data "oasis_cloud_provider" "test_oasis_cloud_providers" {
   organization = "" // Provide your Organization ID here
