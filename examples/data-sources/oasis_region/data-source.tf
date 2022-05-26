d124431b5304daf3997ae5c8f4603b0809f10fe5
Adonis Murati <adonis.murati@arangodb.com>
2022-05-26 11:39:06 +0200

    OAS-4846 | add readme file with region data source config

--- examples/data-sources/oasis_region/data-source.tf
+++ examples/data-sources/oasis_region/data-source.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    oasis = {
+      source  = "arangodb.com/managed/oasis"
+      version = ">=1.5.1"
+    }
+  }
+}
+
+provider "oasis" {
+  api_key_id     = "" // API Key ID generated in Oasis platform
+  api_key_secret = "" // API Key Secret generated in Oasis platform
+}
+
 // Load in Oasis Cloud Providers
 data "oasis_region" "test_oasis_region" {
   organization = "" // put your organization id here
