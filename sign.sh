dbcf093b1e043e3539e75a0fb22939b3b09b2461
Ewout Prangsma <ewout@prangsma.net>
2022-06-21 11:20:45 +0200

    OAS-4755 | Using base64 encoded key

--- sign.sh
+++ sign.sh
@@ -3,8 +3,7 @@ set -e
 
 gpg-agent --daemon --default-cache-ttl 7200
 # Multiline key in an envvar gets mangled.
-FIXED_KEY=$(echo "$GPG_PRIVATE_KEY" | cat -e | sed 's/\$/\\n/g')
-echo -e $FIXED_KEY | gpg --import --batch --no-tty
+echo "$GPG_PRIVATE_KEY_BASE64" | base64 -d | gpg --import --batch --no-tty
 echo "hello world" > temp.txt
 gpg --detach-sig --yes -v --output=/dev/null --pinentry-mode loopback --passphrase "${PASSPHRASE}" temp.txt
 cd assets ; gpg --detach-sign ${PROJECT}-${VERSION}_SHA256SUMS
