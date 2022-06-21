131b900dbc306913c2b0c3ade97a282992c9afe0
Ewout Prangsma <ewout@prangsma.net>
2022-06-21 11:08:13 +0200

    OAS-4755 | Test sign fix

--- sign.sh
+++ sign.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
 
 gpg-agent --daemon --default-cache-ttl 7200
-echo "$GPG_PRIVATE_KEY" | cat -e | sed 's/\$/\\n/g' | gpg --import --batch --no-tty
+# Multiline key in an envvar gets mangled.
+FIXED_KEY=$(echo "$GPG_PRIVATE_KEY" | cat -e | sed 's/\$/\\n/g')
+echo -e $FIXED_KEY | gpg --import --batch --no-tty
 echo "hello world" > temp.txt
 gpg --detach-sig --yes -v --output=/dev/null --pinentry-mode loopback --passphrase "${PASSPHRASE}" temp.txt
 cd assets ; gpg --detach-sign ${PROJECT}-${VERSION}_SHA256SUMS
