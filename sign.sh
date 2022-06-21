17c39bfc9e8f6a7ab91fbedded93c86419664c8f
Ewout Prangsma <ewout@prangsma.net>
2022-06-21 10:49:17 +0200

    OAS-4755 | Test sign fix

--- sign.sh
+++ sign.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gpg-agent --daemon --default-cache-ttl 7200
-echo "${GPG_PRIVATE_KEY}" | gpg --import --batch --no-tty
+echo "$GPG_PRIVATE_KEY" | cat -e | sed 's/\$/\\n/g' | gpg --import --batch --no-tty
 echo "hello world" > temp.txt
 gpg --detach-sig --yes -v --output=/dev/null --pinentry-mode loopback --passphrase "${PASSPHRASE}" temp.txt
 cd assets ; gpg --detach-sign ${PROJECT}-${VERSION}_SHA256SUMS
